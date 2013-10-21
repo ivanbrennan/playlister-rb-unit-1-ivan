8020b6aaa7d8cefecd8bc8e61ed2e44039e1a830
ivanbrennan <ivan.brennan@gmail.com>
2013-10-21 09:39:00 -0400

    Complete specs and models

--- lib/models/song.rb
+++ lib/models/song.rb
@@ -1,5 +1,19 @@
 class Song
+  attr_accessor :name, :artist
   attr_reader :genre
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_songs
+    all.clear
+  end
+
+  def initialize
+    @@all << self
+  end
 
   def genre=(g)
     @genre = g
--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -1,27 +1,43 @@
-#require "spec_helper"
+require "spec_helper"
 
 describe "Song" do
 
-  #let (:song) { Song.new }
+  let (:song) { Song.new }
+  let (:artist) { Artist.new }
+  let (:genre) { Genre.new }
 
   it "can initialize a song" do
+    expect(song).to be_an_instance_of(Song)
   end
 
   it "has a name" do
+    song.name = "Spottieottiedopaliscious"
+    expect(song.name).to eq("Spottieottiedopaliscious")
   end
 
   it "has an artist" do
+    song.artist = artist
+    expect(song.artist).to eq(artist)
   end
 
   it "has a genre" do
+    song.genre = genre
+    expect(song.genre).to eq(genre)
   end
 
   describe "class methods" do
 
+    before(:each) { Song.reset_songs }
+
     it "keeps track of all created songs" do
+      song
+      expect(Song.all.count).to eq(1)
     end
 
     it "can reset the existing songs" do
+      song
+      Song.reset_songs
+      expect(Song.all.count).to eq(0)
     end
 
   end
